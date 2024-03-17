eca59f6d9ee76ebe197755d0a882a79fd2af8991
Stefan Dudu <stefan01.dev@gmail.com>
2024-03-17 16:17:13 +0200

    Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,6 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <SpeedInsights />
+    <Analytics />
     <Provider store={store}>
       <BrowserRouter>
         <App />
