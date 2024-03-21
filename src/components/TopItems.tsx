9912349fc121aacd71129adb7a0a415ad7be042f
Stefan Dudu <stefan01.dev@gmail.com>
2024-03-21 17:03:19 +0200

    top beans linking

--- src/components/TopItems.tsx
+++ src/components/TopItems.tsx
@@ -1,153 +1,85 @@
-import React from "react";
-import bean1 from "../assets/bean1.jpg";
-import bean2 from "../assets/bean2.jpg";
-import bean3 from "../assets/bean3.jpg";
+import React, { SyntheticEvent, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./TopItems.scss";
-import logo from "../assets/logo2.png";
 
-type Props = {};
+type Coffee = {
+  _id: string;
+  name: string;
+  origin: string;
+  roastLevel: string;
+  flavorNotes: string[];
+  aroma: string;
+  acidity: number;
+  body: number;
+  price: number;
+  ratingsAverage: number;
+  ratingsQuantity: number;
+  summary: string;
+  image: string;
+  locations: {
+    type: string;
+    coordinates: number[];
+    description: string;
+    _id: string;
+    id: string;
+  }[];
+  slug: string;
+  id: string;
+};
 
-const TopItems = (props: Props) => {
+const TopItems = (props: any) => {
+  const [data, setData] = useState<Coffee[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchDataForPosts = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_LOCAL_URL}api/v1/beans/top-7`
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error: Status ${response.status}`);
+        }
+        const { data } = await response.json();
+        setData(data.data);
+        setError(null);
+      } catch (err: any) {
+        setData(null);
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDataForPosts();
+  }, []);
+
+  function addDefaultSrc(e: SyntheticEvent<HTMLImageElement, Event>) {
+    e.currentTarget.src =
+      "https://baristretto-bucket.s3.eu-central-1.amazonaws.com/catchBeanBag.jpg";
+  }
+
   return (
     <div>
       <h2>Top beans </h2>
       <div className="container">
-        <div className="item">
-          {
-            <img
-              src={bean1}
-              width="200"
-              height="200"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          }
-        </div>
-        <div className="item">
-          {
-            <img
-              src={bean2}
-              width="200"
-              height="200"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          }
-        </div>
-        <div className="item">
-          {
-            <img
-              src={bean3}
-              width="200"
-              height="200"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          }
-        </div>
-        <div className="item">
-          {
-            <img
-              src={
-                "https://baristretto-bucket.s3.eu-central-1.amazonaws.com/ethiopian-yirgacheffe.jpg"
-              }
-              width="200"
-              height="200"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          }
-        </div>
-        <div className="item">
-          {
-            <img
-              src={
-                "https://baristretto-bucket.s3.eu-central-1.amazonaws.com/kenyan-aa.jpg"
-              }
-              width="200"
-              height="200"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          }
-        </div>
-        <div className="item">
-          {
-            <img
-              src={bean3}
-              width="200"
-              height="200"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          }
-        </div>{" "}
-        <div className="item">
-          {
-            <img
-              src={bean1}
-              width="200"
-              height="200"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          }
-        </div>
-        <div className="item">
-          {
-            <img
-              src={bean2}
-              width="200"
-              height="200"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          }
-        </div>
-        <div className="item">
-          {
-            <img
-              src={bean3}
-              width="200"
-              height="200"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          }
-        </div>{" "}
-        <div className="item">
-          {
-            <img
-              src={bean1}
-              width="200"
-              height="200"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          }
-        </div>
-        <div className="item">
-          {
-            <img
-              src={bean2}
-              width="200"
-              height="200"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          }
-        </div>
-        <div className="item">
-          {
-            <img
-              src={bean3}
-              width="200"
-              height="200"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          }
-        </div>
+        {data &&
+          data?.map((el) => (
+            <Link to={`/coffee/${el._id}`}>
+              <div className="item">
+                <img
+                  src={el?.image}
+                  width="200"
+                  height="200"
+                  className="d-inline-block align-top"
+                  alt="React Bootstrap logo"
+                  onError={addDefaultSrc}
+                />
+              </div>
+              {el?.name}
+            </Link>
+          ))}
       </div>
     </div>
   );
