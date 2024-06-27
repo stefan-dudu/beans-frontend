c31157295021abe7939c0f9a0ccf191319ad2430
Stefan Dudu <stefan01.dev@gmail.com>
2024-06-27 15:06:38 +0300

    video change for safari

--- src/components/heroSection/FirstSection.tsx
+++ src/components/heroSection/FirstSection.tsx
@@ -10,7 +10,16 @@ const FirstSection = (props: Props) => {
   return (
     <div className="video-parent-wrapper">
       <h1>Coffee reviews and informations</h1>
-      <video className="video" id="video" autoPlay loop muted preload="auto">
+      <video
+        className="video"
+        id="video"
+        autoPlay
+        loop
+        controls={false}
+        playsInline
+        muted
+        preload="auto"
+      >
         <source src="https://baristretto-bucket.s3.eu-central-1.amazonaws.com/misc/video1-1920.mp4" />
       </video>
       <Button
